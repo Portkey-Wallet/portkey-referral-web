5f60db247e2ff34214e9169e4354b664857185a3
yangkexin <yangkexin@portkey.finance>
2024-01-19 16:51:22 +0800

    feat: 🎸 add viewport

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,14 +6,14 @@ export const metadata: Metadata = {
   title: 'Portkey: A User-Friendly and Secure AA Wallet for Asset Management and Web3 Experience',
   description: `Portkey is the first account abstraction (AA) wallet from aelf's ecosystem. Users can swiftly log into Portkey via their Web2 social info with no private keys or mnemonics required. Account creation is free for all users!`,
   keywords: ['DID', 'social recovery', 'crypto wallet', 'AA wallet'],
-  viewport: {
-    viewportFit: 'cover',
-    width: 'device-width',
-    initialScale: 1,
-    userScalable: false,
-    minimumScale: 1,
-    maximumScale: 1,
-  },
+};
+
+export const viewport = {
+  viewportFit: 'cover',
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: 'no',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
