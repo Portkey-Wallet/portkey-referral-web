2b5ca2ff71966d5f64e657b4cca23a51bdeecff8
yangkexin <yangkexin@portkey.finance>
2024-01-19 22:14:39 +0800

    feat: 🎸 change network

--- rewrites.js
+++ rewrites.js
@@ -1,10 +1,10 @@
 module.exports = [
   // test3-v2
-  { source: '/api/:path*', destination: 'http://192.168.64.201:5001/api/:path*' },
-  { source: '/connect/:path*', destination: 'http://192.168.64.201:8080/connect/:path*' },
+  { source: '/api/:path*', destination: 'http://192.168.67.127:5001/api/:path*' },
+  { source: '/connect/:path*', destination: 'http://192.168.67.127:8080/connect/:path*' },
   {
     source: '/graphql/:path*',
-    destination: 'http://192.168.67.51:8083/AElfIndexer_DApp/PortKeyIndexerCASchema/graphql/:path*',
+    destination: 'http://192.168.67.99:8083/AElfIndexer_DApp/PortKeyIndexerCASchema/graphql/:path*',
   },
   { source: '/cms/:path*', destination: 'https://localtest-applesign.portkey.finance/cms/:path*' },
 ];
