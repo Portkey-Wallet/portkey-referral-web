e97bdeaa6bfed9873c1772886b056f55da3cc64c
semaphoresentinel <149366454+semaphoresentinel@users.noreply.github.com>
2024-06-18 18:30:06 +0800

    feat: add access token

--- src/app/test-wallet/page.tsx
+++ src/app/test-wallet/page.tsx
@@ -1,11 +1,12 @@
 'use client';
+import useAccount from '@/hooks/useAccount';
 import useDiscoverProvider from '@/hooks/useDiscoverProvider';
 import { getCaHashAndOriginChainIdByWallet } from '@/utils/portkey';
 import { useConnectWallet } from '@aelf-web-login/wallet-adapter-react';
 import { did } from '@portkey/did-ui-react';
 import {Button} from 'antd'
+import { log } from 'console';
 import { useState } from 'react';
-
 const AElf = require('aelf-sdk');
 
 export default function Referral() {
@@ -13,6 +14,7 @@ export default function Referral() {
 
   did.services.communityRecovery.getHolderInfoByManager
   const { connectWallet, disConnectWallet, walletInfo, walletType } = useConnectWallet();
+  const { login, sync, logout, isConnected, isLocking} = useAccount();
   const onConnectBtnClickHandler = async ()=>{
     console.log('wfs~~~');
       try {
@@ -41,11 +43,26 @@ const { getSignatureAndPublicKey } = useDiscoverProvider();
     }}>getSignatureAndPublicKey</Button>
     <Button onClick={onConnectBtnClickHandler}>Connect</Button>
      <Button onClick={onDisConnectBtnClickHandler}>DisConnect</Button>
+     <Button onClick={async () => {
+      const status = await login();
+      // const caHash = await sync();
+      console.log('login success! caHash', caHash);
+     }}>Login And Sync</Button>
+      <Button onClick={async () => {
+      const caHash = await sync();
+      console.log('sync success!', caHash);
+     }}>Sync</Button>
+     <Button onClick={async () => {
+      await logout();
+     }}>Logout</Button>
      <Button onClick={() => {
       console.log('walletInfo', walletInfo);
+      console.log('walletInfo', isConnected);
+      console.log('walletInfo2', did.didWallet);
       // setwalletInfo(walletInfo);
      }}>walletInfo</Button>
      
      <div style={{color: 'white'}}>walletInfo: {walletInfo?.address}</div>
+     <div style={{color: 'white'}}>isConnected: {isConnected+''}</div>
   </div>;
 }
--- src/hooks/useAccount.ts
+++ src/hooks/useAccount.ts
@@ -1,22 +1,56 @@
 import { useConnectWallet } from '@aelf-web-login/wallet-adapter-react';
 import { useCallback } from 'react';
 import { getCaHashAndOriginChainIdByWallet } from '@/utils/portkey';
-
+import { getConnectToken } from '@/utils/axios';
+import useDiscoverProvider from './useDiscoverProvider';
+import { ConnectHost } from '@/constants/network';
+const AElf = require('aelf-sdk');
 export default function useAccount() {
   const { connectWallet, disConnectWallet, walletInfo, walletType, isConnected, isLocking } = useConnectWallet();
+  const { getSignatureAndPublicKey } = useDiscoverProvider();
+  console.log('1111wallet is:', walletInfo);
+  console.log('1111walletType is:', walletType);
   const login = useCallback(async () => {
     try {
       const rs = await connectWallet();
-      const { caHash } = await getCaHashAndOriginChainIdByWallet(walletInfo, walletType);
+      return true;
+    } catch (e: any) {
+      console.log('connect failed', e.message)
+      return false;
+    }
+  }, [connectWallet]);
+  const sync = useCallback(async () => {
+    try {
+      if(!isConnected) {
+        console.error('please connect wallet first!');
+        return;
+      }
+    const timestamp = Date.now();
+    // const signInfo = Buffer.from(`${walletInfo?.address}-${timestamp}`).toString('hex');
+
+      const signInfo =  AElf.utils.sha256(`${walletInfo?.address}-${timestamp}`)
+      const { caHash, originChainId } = await getCaHashAndOriginChainIdByWallet(walletInfo, walletType);
+      const { pubKey, signatureStr } = await getSignatureAndPublicKey(signInfo);
+      console.log("caHash===", caHash);
+      console.log("originChainId===", originChainId);
+      getConnectToken({
+        grant_type: 'signature',
+        client_id: 'CAServer_App',
+        scope: 'CAServer',
+        signature: signatureStr || '',
+        pubkey: pubKey|| '',
+        timestamp,
+        ca_hash: caHash,
+        chainId: originChainId,
+      })
       return caHash;
-      console.log('connect success', rs);
     } catch (e: any) {
       console.log('connect failed', e.message)
     }
-  }, [connectWallet, walletInfo, walletType]);
+  }, [getSignatureAndPublicKey, isConnected, walletInfo, walletType]);
   const logout = useCallback(async () => {
       await disConnectWallet();
   }, [disConnectWallet]);
 
-  return { login, logout, isConnected, isLocking };
+  return { login, sync, logout, isConnected, isLocking };
 }
